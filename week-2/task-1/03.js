9c61f079c2fbd0668df93d548b3daace54d63d07
Kanak Sharma <kanaksharma74665@gmail.com>
2025-02-08 15:56:32 +0530

    coded

---
+++ week-2/task-1/03.js
@@ -0,0 +1,8 @@
+console.log("Find the Largest Number in an Array");
+
+function findLargestNumber(numbers) {
+    return Math.max(...numbers);
+}
+
+let numArray = [10, 25, 60.55, 5.5, 60.5]; 
+console.log(`The largest number is: ${findLargestNumber(numArray)}`);
