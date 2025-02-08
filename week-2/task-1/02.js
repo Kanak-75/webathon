921abd6dc6ce99cdb401e73c7a10c4ef2d19b16f
Kanak Sharma <kanaksharma74665@gmail.com>
2025-02-08 15:49:29 +0530

    coded

---
+++ week-2/task-1/02.js
@@ -0,0 +1,8 @@
+console.log("Temperature Conversion: Celsius to Fahrenheit");
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+let celsiusTemp = 88; 
+console.log(`${celsiusTemp}°C is equal to ${celsiusToFahrenheit(celsiusTemp)}°F`);
