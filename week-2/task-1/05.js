c4e4ab8c0814c758ca7c83ccbc4975a7317dd106
Kanak Sharma <kanaksharma74665@gmail.com>
2025-02-08 16:20:29 +0530

    added

---
+++ week-2/task-1/05.js
@@ -0,0 +1,10 @@
+--undefined (Variable Declared but Not Assigned)--
+1. A variable is declared but not assigned a value.
+2. A function without a return statement returns undefined.
+3. An object property that does not exist is undefined.
+                
+                while 
+--null (Explicitly Empty Value)--
+1. null is manually assigned to a variable to indicate "no value" or "empty value".
+2. It is an intentional absence of value.              
+                   
