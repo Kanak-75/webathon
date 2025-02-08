49753e7b0e68dac5ccdf618fd30f8a2ab35cbdaa
Kanak Sharma <kanaksharma74665@gmail.com>
2025-02-08 19:22:50 +0530

    added

---
+++ week-2/task-1/12.js
@@ -0,0 +1,11 @@
+function printMultiplicationTable(num)
+ {
+    let i = 1;
+    while (i <= 10) {
+        console.log(`${num} x ${i} = ${num * i}`);
+        i++;
+    }
+}
+
+printMultiplicationTable(9);
+
